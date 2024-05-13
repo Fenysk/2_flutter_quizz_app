435510b9eafcc66b10393a933a2d27fe2477f5a2
Fenysk <minefenysk@gmail.com>
2024-05-13 12:49:21 +0200

    💄 Use AnswerButton widget and styling components

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quizz_app/models/answer_button.dart';
 
 class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key});
@@ -12,6 +13,44 @@ class QuestionScreen extends StatefulWidget {
 class _QuestionScreenState extends State<QuestionScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Question Screen');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Question 1',
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.bold,
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(height: 40),
+          Wrap(
+            direction: Axis.vertical,
+            spacing: 10,
+            children: [
+              AnswerButton(
+                answerText: 'Réponse 1',
+                onTap: () {},
+              ),
+              AnswerButton(
+                answerText: 'Réponse 2',
+                onTap: () {},
+              ),
+              AnswerButton(
+                answerText: 'Réponse 3',
+                onTap: () {},
+              ),
+              AnswerButton(
+                answerText: 'Réponse 4',
+                onTap: () {},
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
   }
 }
